94b4dc4d283a8e546582306bd78479158fccee37
githud33 <githud33@gmail.com>
2026-06-06 14:49:44 +0700

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,5 @@
+﻿var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/Bunny/ไฟล์หลัก/[ชื่อโปรเจกต์ของคุณ]?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
---
+++ ล้างแคช/11-pb.js_v=1.js
@@ -0,0 +1,5 @@
+﻿var script = document.createElement('script');
+// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
+script.src = 'https://githud33.github.io/Bunny/ไฟล์หลัก/11-pb.js_v=1.js?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
