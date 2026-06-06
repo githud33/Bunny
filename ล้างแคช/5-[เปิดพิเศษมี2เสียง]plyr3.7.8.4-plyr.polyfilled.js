47fb652f3a59497177a1f8d0b3fc1583f2040f5a
githud33 <githud33@gmail.com>
2026-06-06 16:19:07 +0700

    Delete

--- ล้างแคช/5-[เปิดพิเศษมี2เสียง]plyr3.7.8.4-plyr.polyfilled.js
+++
@@ -1,5 +0,0 @@
-﻿var script = document.createElement('script');
-// ชี้ทางไปหาไฟล์ตัวจริงในโฟลเดอร์ core
-script.src = 'https://githud33.github.io/Bunny/ไฟล์หลัก/5-[เปิดพิเศษมี2เสียง]plyr3.7.8.4-plyr.polyfilled.js?t=' + new Date().getTime();
-script.defer = true;
-document.head.appendChild(script);
--- ไฟล์หลัก/16-จำเวลาplyr-position.js
+++
@@ -1,1 +0,0 @@
-// function initPlyrPositionSaver(player, videoId) {
